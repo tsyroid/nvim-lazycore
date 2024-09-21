e1f6a54352a31230c2290c9e0d54485f1762da8f
Tom Syroid <tsyroid@gmail.com>
2024-09-21 10:50:19 -0700

    (Add) My default keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,30 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+
+-- shorten function name
+local keymap = vim.keymap.set
+
+-- Select complete document
+keymap("n", "<C-a>", "ggVG", opts)
+
+-- Resize window with +/-
+keymap("n", "-", "<C-w>5<", opts)
+keymap("n", "+", "<C-w>5>", opts)
+
+-- `jj` to exit insert mode without hitting Esc
+keymap("i", "jj", "<Esc>", opts)
+
+-- keep indents active
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move lines up/down
+keymap("v", "K", ":m '>-2<CR>gv=gv", { desc = "Move current line up" })
+keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move current line down" })
+
+-- 'H'/'L' to jump start/end of line
+keymap("n", "H", "^", { desc = "Move cursor to start (first char) of line" })
+keymap("n", "L", "$", { desc = "Move cursor to end (last char) of line" })
